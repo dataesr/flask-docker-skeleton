f4e202122391d768018f4521693357b502231cdb
victor barbier <victor.barbier@recherche.gouv.fr>
2024-01-30 10:20:59 +0100

    feat(bso): test task

--- mycode/bso_coverage/bso_coverage.py
+++ mycode/bso_coverage/bso_coverage.py
@@ -6,6 +6,7 @@ def test_mother_duck():
     error = "NOT CONNECTED"
     with duckdb.connect(f"md:?motherduck_token={os.getenv('MD_TOKEN')}") as con:
         error = "CONNECTED"
-        con.sql("UPDATE openalex_2021 set coverage.last_state = 'UNDEFINED' WHERE doi IS NULL")
-        con.sql("UPDATE openalex_2021 set coverage.last_error = 'ALEX_DOI_NOT_FOUND' WHERE doi IS NULL")
+        con.sql(
+            "UPDATE openalex_2021 SET  coverage = STRUCT_PACK(last_error := 'ALEX_DOI_NOT_FOUND', last_error_data := coverage.last_error_data, last_state := 'UNDEFINED') WHERE doi IS NULL"
+        )
     return error
